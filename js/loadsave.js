b3a20bbbbc2423a6fb0de972d98a5cdee390d25a
JulezLovesCode <julz07@tuta.io>
2024-06-24 15:24:02 +0200

    first commit

---
+++ js/loadsave.js
@@ -0,0 +1,13 @@
+// function save() {
+//   let cartitemsAsText = JSON.stringify(cartitems);
+
+//   localStorage.setItem('cartitems', cartitemsAsText);
+// }
+
+// function load() {
+//   let cartitemsAsText = localStorage.getItem('cartitems');
+
+//   if (cartitemsAsText) {
+//     cartitems = JSON.parse(cartitemsAsText);
+//   }
+// }
